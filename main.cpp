35c805b60320ce888a967bfdb3b0c35e8b174250
toby-schonrock <tobias@schonrocks.com>
2022-06-28 16:35:37 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <numbers>
+#include <string>
+
+#include "SFML/Graphics.hpp"
+#include "imgui-SFML.h"
+#include "imgui.h"
+
+int main() {
+    std::cout << "hello world \n";
+    return 0;
+}
