66dab13103aacbaa7574586a049c18932b37fe61
toby-schonrock <tobias@schonrocks.com>
2022-06-28 23:35:47 +0100

    Working display
    yey

--- main.cpp
+++ main.cpp
@@ -6,8 +6,6 @@
 #include <string>
 #include <vector>
 
-#include "SFML/System/Vector2.hpp"
-#include "SFML/Window/WindowStyle.hpp"
 #include "Vector2.hpp"
 #include "SFML/Graphics.hpp"
 #include "imgui-SFML.h"
@@ -25,30 +23,30 @@ class Snake {
     public:
         Snake(const Vec2I& pos_, const Vec2I& gridSize_) : pieces({pos_}), gridSize(gridSize_) {}
 
-        void move(){
-            pieces[head] += direction;
+    void move(){
+        pieces[head] += direction;
+    }
+
+    void draw(sf::RenderWindow& window, sf::RectangleShape rec){
+        for(const auto& p: pieces) {
+            rec.setPosition(p.x * 10, p.y * 10);
+            window.draw(rec);
+        }
     }
 };
 
-void displaySnake(const Snake& snake, sf::RenderWindow& window){
-    sf::RectangleShape rec(sf::Vector2f(10, 10));
-    rec.setFillColor(sf::Color::Green);
-    for(const auto& p: snake.pieces) {
-        rec.setPosition(p.x * 10, p.y * 10);
-        window.draw(sf::RectangleShape());
-        // std::cout << "square drawn \n";
-    }
-}
-
 int main() {
     const Vec2I gridSize(50,50);
-    Snake snake(Vec2I(10, 10), gridSize);
+    Snake snake(Vec2I(1, 1), gridSize);
 
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
-
     sf::RenderWindow window(sf::VideoMode(500,500), "Snake", sf::Style::Default, settings); // sf::VideoMode::getDesktopMode(), sf::Style::Fullscreen old one
     ImGui::SFML::Init(window);
+
+    sf::RectangleShape body;
+    body.setSize(sf::Vector2f(10, 10));
+    body.setFillColor(sf::Color::Green);
 
     while (window.isOpen()) {
         sf::Event event; //NOLINT
@@ -56,13 +54,14 @@ int main() {
             ImGui::SFML::ProcessEvent(event);
             if (event.type == sf::Event::Closed) window.close();
         }
-        window.clear();
-        displaySnake(snake, window);
+        // window.clear();
+        snake.draw(window, body);
+
         window.display();
     }
 
     
 
-    std::cout << "hello world \n";
+    std::cout << "bye :)";
     return 0;
 }
